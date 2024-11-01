2535dd9cf01b182a456f003e126f2e48fd9b41c7
Gervasio Benardo Chavane <138519088+Chavane798@users.noreply.github.com>
2024-11-01 19:26:26 +0200

    trabalhando com o SEO

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import { ref, listAll, getDownloadURL } from "firebase/storage";
 import { storage } from '../components/Data_Base_API';
+import Head from 'next/head';
 
 function App() {
     const [currentPath, setCurrentPath] = useState(""); 
@@ -30,7 +31,29 @@ function App() {
     }, [currentPath]);
 
     return (
-        <div className="container mx-auto p-4 ">
+        <>
+            <Head>
+                <title>Mundo da Programação - Biblioteca Online de Livros de Programação</title>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="description" content="Explore um vasto acervo de livros sobre programação, acessível a todos os níveis de conhecimento, desde iniciantes até desenvolvedores avançados." />
+                <meta name="keywords" content="programação, livros, JavaScript, Python, React, Next.js, desenvolvimento web" />
+                <meta name="author" content="Gervásio Bernardo Chavane" />
+                <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+                
+                <meta property="og:title" content="Mundo da Programação" />
+                <meta property="og:description" content="Acesse uma biblioteca online gratuita com livros sobre programação." />
+                <meta property="og:url" content="https://livraria-pro.vercel.app/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="URL da imagem que representa o site" />
+                
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Mundo da Programação" />
+                <meta name="twitter:description" content="Acesse uma biblioteca online gratuita com livros sobre programação." />
+                <meta name="twitter:image" content="URL da imagem que representa o site" />
+            </Head>
+
+            <div className="container mx-auto p-4 ">
             <h1 className="text-3xl text-center m-4 gap-4 text-7xl ">Bem-vindo à minha biblioteca!</h1>
             <div className="justify-center m-6 bg-slate-100 p-8 raunded">
                 <p>Explore um vasto acervo de livros sobre programação, onde você encontrará 
@@ -57,7 +80,7 @@ function App() {
                             <a 
                                 href={item.url} 
                                 download 
-                                className="p-1 ring-offset-2 ring-4 rounded block text-center"
+                                className="p-1 ring-offset-2 ring-4 rounded block text-center "
                             >
                                 Download
                             </a>
@@ -66,7 +89,9 @@ function App() {
                 ))}
             </ul>
         </div>
-    );
+    
+        </>
+    );       
 }
 
 export default App;
